536546c597e3a8d762729a301616b40b24d67c0f
prozzzzgamer <amiinn.lulicami878@gmail.com>
2023-05-03 17:28:15 +0200

    Prvi korak:   - u prvom koraku programa ti samo napravi kafu;   - treba ispisati u izlaz šta radi dok pravi kafu.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Početak pravljenja kafe.");
+        System.out.println("Mlijevenje zrna kafe.");
+        System.out.println("Prokuhavanje vode.");
+        System.out.println("Mješanje prokuhane vode sa zdrobljenim zrnima kafe.");
+        System.out.println("Sipanje kape u šolju.");
+        System.out.println("Sipanje malo mlijeka u šolju.");
+        System.out.println("Kafa je spremna!");
+    }
+}
