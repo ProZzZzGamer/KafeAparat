62f83ce9b87d674f70e6e589b505b0ad2621c6f4
prozzzzgamer <amiinn.lulicami878@gmail.com>
2023-05-09 18:02:26 +0200

    Treći korak/Procijena broja šolji kafe:     1. Opis: U ovom koraku program provjerava količinu vode, mlijeka i kafe u aparatu.     2. Ciljevi: U ovom koraku program radi slijedeće:                 1. Zatraži količinu vode, mlijeka i zrna kafe dostupnih u momentu i onda pita za broj šolji koje korisnik treba;                 2. Ako aparat ima dovoljno sastojaka da napravi tu količinu kafe, program treba ispisati "Da, mogu napraviti tu količinu kafe";                 3. Ako aparat može napraviti više od toga, program treba ispisati "Da, mogu napraviti tu količinu kafe (i čak N više od toga)", gdje je N broj dodatnih šolja kafe koje aparat može napraviti;                 4. Ako aparat nema dovoljno resursa da napravi zatraženu količinu kafe, program treba ispisati "Ne, mogu napraviti samo N šolji kafe"
    (Kao u prošlom koraku aparat treba 200 ml vode, 50 ml mlijeka i 15 g zrna kafe da bi napravio jednu šolju.)

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner unos = new Scanner(System.in);
 
+        System.out.println("Napiši koliko ml vode aparat ima: ");
+        int kolicinaVode = unos.nextInt();
+        System.out.println("Napiši koliko ml mlijeka aparat ima: ");
+        int kolicinaMlijeka = unos.nextInt();
+        System.out.println("Napiši koliko grama zrna kafe aparat ima: ");
+        int kolicinaZrnaKafe = unos.nextInt();
+
         System.out.println("Napiši koliko šolja kafe ti treba: ");
-        int šolje = unos.nextInt();
+        int solje = unos.nextInt();
 
-        int voda = 200 * šolje;
-        int mlijeko = 50 * šolje;
-        int zrnaKafe = 15 * šolje;
+        int brojsolja = 0;
+        while (kolicinaVode >= 200 && kolicinaMlijeka >= 50 && kolicinaZrnaKafe >= 15) {
+            kolicinaVode = kolicinaVode - 200;
+            kolicinaMlijeka = kolicinaMlijeka - 50;
+            kolicinaZrnaKafe = kolicinaZrnaKafe - 15;
+            brojsolja++;
+        }
 
-        System.out.printf("Za %d šolja kafe ti treba: ", šolje);
-        System.out.printf("%n %d ml vode", voda);
-        System.out.printf("%n %d ml mlijeka", mlijeko);
-        System.out.printf("%n %d g zrna kafe", zrnaKafe);
+        if (brojsolja < solje) {
+            System.out.printf("Ne, mogu napraviti samo %d solje kafe", brojsolja);
+        } else if (brojsolja > solje) {
+            System.out.printf("Da, mogu napraviti tu količinu kafe (i čak %d više od toga)", brojsolja - solje);
+        } else if (brojsolja == solje) {
+            System.out.println("Da, mogu napraviti tu količinu kafe");
+        }
     }
 }
