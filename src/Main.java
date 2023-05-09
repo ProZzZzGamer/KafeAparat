cd2f2dbb791c449ebed47ffa41032625db978abb
prozzzzgamer <amiinn.lulicami878@gmail.com>
2023-05-09 22:41:37 +0200

    Peti korak/Praćenje zaliha:     1. Opis: Program treba uzastopno pitati korisnika šta želi uraditi, ako korisnik napiše "kupi", "napuni", "uzmi", onda program treba uraditi isto što je radio u prošloj fazi. Uz to, ako korisnik želi isključiti aparat, treba napisati "ugasi". Program se treba ugasiti nakon te komande. Isto, ako korisnik upiše "zalihe", program treba ispisati sve resurse koje aparat ima. To znači da više ne trebaš prikazivati resurse na početku/kraju programa.     2. Ciljevi: U ovom koraku program će raditi beskonačno da bi napravio kafu dok ne dobije signal za gašenje. Dodaj dvije nove opcije: "zalihe" i "ugasi".     I još jedno poboljšanje, ako korisnik krene kupiti kafu tako što napiše "kupi" i onda se predomisli, mora moći napisati "nazad" i vratiti se u glavni ciklus.

--- src/Main.java
+++ src/Main.java
@@ -10,73 +10,87 @@ public class Main {
         int solje = 9;
         int pare = 225;
 
-        System.out.println("Aparat za kafu ima: ");
-        System.out.printf("%d ml vode %n", voda);
-        System.out.printf("%d ml mlijeka %n", mlijeko);
-        System.out.printf("%d g zrna kafe %n", zrna);
-        System.out.printf("%d jednokratnih šolji %n", solje);
-        System.out.printf("%d KM %n", pare);
-
-        System.out.println("Napiši radnju (kupi, napuni, uzmi): ");
+        System.out.println("Napiši radnju (kupi, napuni, uzmi, zalihe, ugasi): ");
 
         String radnja = unos.next();
+        while (!radnja.equals("ugasi")) {
+            switch (radnja) {
+                case "kupi" -> {
 
-        switch (radnja) {
-            case "kupi" -> {
+                    System.out.println("Šta želiš kupiti? 1 - espreso, 2 - latte, 3 - kapučino: ");
 
-                System.out.println("Šta želiš kupiti? 1 - espreso, 2 - latte, 3 - kapučino: ");
+                    String kupnja = unos.next();
 
-                int kupnja = unos.nextInt();
+                    while (!kupnja.equals("1") && !kupnja.equals("2") && !kupnja.equals("3") && !kupnja.equals("nazad")) {
+                        System.out.println("Unio si opciju koja ne postoji, unesi ponovo: ");
+                        kupnja = unos.next();
+                    }
 
-                while (kupnja > 3) {
-                    System.out.println("Unio si opciju koja ne postoji, unesi ponovo: ");
-                    kupnja = unos.nextInt();
+                    switch (kupnja) {
+                        case "1" -> {
+                            if (voda >= 250 & zrna >= 16 & solje >= 1) {
+                                voda = voda - 250;
+                                zrna = zrna - 16;
+                                solje = solje - 1;
+                                pare = pare + 2;
+                                System.out.println("Imam resuresa, pravim kahvu.");
+                            } else if (voda < 250 || zrna < 16 || solje < 1) {
+                                System.out.println("Nema dovoljno sastojaka.");
+                            }
+                        }
+                        case "2" -> {
+                            if (voda >= 350 & mlijeko >= 75 & zrna >= 20 & solje >= 1) {
+                                voda = voda - 350;
+                                mlijeko = mlijeko - 75;
+                                zrna = zrna - 20;
+                                solje = solje - 1;
+                                pare = pare + 4;
+                                System.out.println("Imam resuresa, pravim kahvu.");
+                            } else if (voda < 350 || mlijeko < 75 || zrna < 20 || solje < 1) {
+                                System.out.println("Nema dovoljno sastojaka.");
+                            }
+                        }
+                        case "3" -> {
+                            if (voda >= 200 & mlijeko >= 100 & zrna >= 12 & solje >= 1) {
+                                voda = voda - 200;
+                                mlijeko = mlijeko - 100;
+                                zrna = zrna - 12;
+                                solje = solje - 1;
+                                pare = pare + 3;
+                                System.out.println("Imam resuresa, pravim kahvu.");
+                            } else if (voda < 200 || mlijeko < 100 || zrna < 12 || solje < 1) {
+                                System.out.println("Nema dovoljno sastojaka.");
+                            }
+                        }
+                        case "nazad" -> {
+                        }
+                    }
                 }
-
-                switch (kupnja) {
-                    case 1 -> {
-                        voda = voda - 250;
-                        zrna = zrna - 16;
-                        solje = solje - 1;
-                        pare = pare + 2;
-                    }
-                    case 2 -> {
-                        voda = voda - 350;
-                        mlijeko = mlijeko - 75;
-                        zrna = zrna - 20;
-                        solje = solje - 1;
-                        pare = pare + 4;
-                    }
-                    case 3 -> {
-                        voda = voda - 200;
-                        mlijeko = mlijeko - 100;
-                        zrna = zrna - 12;
-                        solje = solje - 1;
-                        pare = pare + 3;
-                    }
+                case "napuni" -> {
+                    System.out.println("Napiši koliko ml vode želiš dodati: ");
+                    voda = voda + unos.nextInt();
+                    System.out.println("Napiši koliko ml mlijeka želiš dodati: ");
+                    mlijeko = mlijeko + unos.nextInt();
+                    System.out.println("Napiši koliko grama zrna kafe želiš dodati: ");
+                    zrna = zrna + unos.nextInt();
+                    System.out.println("Napiši koliko jednokratnih šolji želiš dodati: ");
+                    solje = solje + unos.nextInt();
+                }
+                case "uzmi" -> {
+                    System.out.printf("Dao sam ti %d KM %n", pare);
+                    pare = 0;
+                }
+                case "zalihe" -> {
+                    System.out.println("Aparat za kafu ima: ");
+                    System.out.printf("%d ml vode %n", voda);
+                    System.out.printf("%d ml mlijeka %n", mlijeko);
+                    System.out.printf("%d g zrna kafe %n", zrna);
+                    System.out.printf("%d jednokratnih šolji %n", solje);
+                    System.out.printf("%d KM %n", pare);
                 }
             }
-            case "napuni" -> {
-                System.out.println("Napiši koliko ml vode želiš dodati: ");
-                voda = voda + unos.nextInt();
-                System.out.println("Napiši koliko ml mlijeka želiš dodati: ");
-                mlijeko = mlijeko + unos.nextInt();
-                System.out.println("Napiši koliko grama zrna kafe želiš dodati: ");
-                zrna = zrna + unos.nextInt();
-                System.out.println("Napiši koliko jednokratnih šolji želiš dodati: ");
-                solje = solje + unos.nextInt();
-            }
-            case "uzmi" -> {
-                System.out.printf("Dao sam ti %d KM %n", pare);
-                pare = 0;
-            }
+            System.out.println("Napiši radnju (kupi, napuni, uzmi, zalihe, ugasi): ");
+            radnja = unos.next();
         }
-
-        System.out.println("Aparat za kafu ima: ");
-        System.out.printf("%d ml vode %n", voda);
-        System.out.printf("%d ml mlijeka %n", mlijeko);
-        System.out.printf("%d g zrna kafe %n", zrna);
-        System.out.printf("%d jednokratnih šolji %n", solje);
-        System.out.printf("%d KM %n", pare);
     }
 }
